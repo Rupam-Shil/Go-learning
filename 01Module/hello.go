70d17df7ae6387758d06eb1cfc6cf21c20f11939
Rupam Shil <rupamshil111@gmail.com>
2021-09-03 11:24:01 +0530

    for multiple users

--- 01Module/hello.go
+++ 01Module/hello.go
@@ -13,9 +13,12 @@ func main() {
 	log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+
     // Request a greeting message.
 	
-    message, err := greetings.Hello("Gladys")
+    message, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
--- greetings/greetings.go
+++ greetings/greetings.go
@@ -17,10 +17,29 @@ func Hello(name string) (string, error) {
 }
 
 // init sets initial values for variables used in the function.
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+    // A map to associate names with messages.
+    messages := make(map[string]string)
+    // Loop through the received slice of names, calling
+    // the Hello function to get a message for each name.
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        // In the map, associate the retrieved message with
+        // the name.
+        messages[name] = message
+    }
+    return messages, nil
+}
+
+
 func init() {
     rand.Seed(time.Now().UnixNano())
 }
-
 // randomFormat returns one of a set of greeting messages. The returned
 // message is selected at random.
 func randomFormat() string {
