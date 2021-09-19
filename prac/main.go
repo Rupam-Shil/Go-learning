ac185a4d90d1bac52c6486f0dc204bff6f404274
Rupam Shil <rupamshil111@gmail.com>
2021-09-19 10:22:20 +0530

    added

--- prac/main.go
+++ prac/main.go
@@ -1,27 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Welcome to Practice")
-	fmt.Println("What's your name?")
-
-	// reader := bufio.NewReader(os.Stdin)
+	rupam := student{"Rupam", "ENG19CS0265", 5}
+	fmt.Println(rupam)
+	rupam.printUsn()
+	// fmt.Println("Please enter your name")
+	// reader  := bufio.NewReader(os.Stdin)
 	// name, _ := reader.ReadString('\n')
-	// fmt.Println(name)
-	time := time.Now()
-	fmt.Println(time.Format("Monday"))
-	type hola string;
-	var myName hola = "hi there"
-	fmt.Println(myName)
-	const fixedValue = 5;
-	if fixedValue > 10{
-		fmt.Println("greated")
-	} else {
-		fmt.Println("Lesser")
-	}
-	
+	// fmt.Println("Please enter your Gender(m/f)")
+	// gender, _ := reader.ReadString('\n')
+	// gender = strings.TrimSpace(gender)
+	// if gender == "m" {
+	// 	fmt.Println("Welcome Mr.", name)
+	// }else{
+	// 	fmt.Println("Welcome Ms.", name)
+	// }
+
+}
+
+type student struct {
+	name string
+	usn  string
+	year int
+}
+
+func (s student) printUsn() {
+	fmt.Println("The usn is",s.name)
 }
