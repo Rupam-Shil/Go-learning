869066f293a9225713509696c3fcd444ef9a2465
Rupam Shil <rupamshil111@gmail.com>
2021-10-16 10:49:22 +0530

    new  files added

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -90,3 +90,48 @@ func createOneCourse(w http.ResponseWriter, r *http.Request){
 
 	json.NewEncoder(w).Encode(course)
 }
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// first grab id from req
+	params := mux.Vars(r)
+	//loop, id and remove the value and add the value again in the courses with myID
+	for index, course := range courses {
+		if course.CourseId == params["id"]{
+			courses = append(courses[:index],courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("could not find course")
+}
+
+func deleteOneCOurse ( w http.ResponseWriter, r *http.Request){
+	fmt.Println("Time to delete some courses")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("THe json data is empty")
+	}
+
+	for index, course := range courses {
+		if course.CourseId == params["id"]{
+			courses = append(courses[:index], courses[index+1:]...)
+			json.NewEncoder(w).Encode("Successfully deleted an item")
+			break
+		}
+	}
+	json.NewEncoder(w).Encode("COuldn't found the data")
+}
