71d7c9e7102adaf993865f1f17a0d032f8ced83e
Rupam Shil <rupamshil111@gmail.com>
2021-11-07 10:02:21 +0530

    practice done

--- prac/main.go
+++ prac/main.go
@@ -5,54 +5,25 @@ import (
 	"sync"
 )
 
+
 func main() {
-	greeting("Start")
-
-	count := []int{0}
-
 	wg := &sync.WaitGroup{}
+	mt := &sync.Mutex{}
+	score := []int{}
+	wg.Add(3)
 
-	mt :=  &sync.Mutex{}
-
-	wg.Add(4)
-
-	go func (wg *sync.WaitGroup, mt *sync.Mutex) {
-		fmt.Println("In One")
-		mt.Lock()
-		count  = append(count,1)
-		mt.Unlock()
-		wg.Done()
-	}(wg, mt)
-
-	go func (wg *sync.WaitGroup, mt *sync.Mutex)  {
-		fmt.Println("In Two")
-		mt.Lock()
-		count  = append(count,2)
-		mt.Unlock()
-		wg.Done()
-	}(wg, mt)
-
-	go func (wg *sync.WaitGroup, mt *sync.Mutex)  {
-		fmt.Println("In Three")
-		mt.Lock()
-		count  = append(count,3)
-		mt.Unlock()
-		wg.Done()
-	}(wg, mt)
-	
-	go func (wg *sync.WaitGroup, mt *sync.Mutex)  {
-		fmt.Println("In Four")
-		mt.Lock()
-		count  = append(count,4)
-		mt.Unlock()
-		wg.Done()
-	}(wg, mt)
-	
+	go includeScore(&score, 5,wg,mt)
+	go includeScore(&score, 20,wg,mt)
+	go includeScore(&score, 50,wg,mt)
 	wg.Wait()
-	fmt.Println(count)
-	fmt.Println("Finished")
+	fmt.Println(score)
 }
 
-func greeting(s string){
-	fmt.Println(s)
+func includeScore(score *[]int, val int, wg *sync.WaitGroup, mt *sync.Mutex) {
+	for i := 0; i <10 ; i++ {
+		mt.Lock()
+		*score = append(*score, val+i)
+		mt.Unlock()
+	}
+	wg.Done()
 }
